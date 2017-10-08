05dbe4122eb532b9fd4b9a1d8d13eed12265375d
mauriciolobo <mauriciolobo@gmail.com>
2017-10-07 22:42:45 -0400

    Start here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Bootstrap Sass boilerplate';
+  staticAlertOpened = true;
 }
